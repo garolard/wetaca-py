4001cf737b74b00a30c9e97be47a3c9ab5a615e9
Gabriel Ferreiro <gbril9119@gmail.com>
2018-10-24 22:39:17 +0200

    Parsings y expresiones regulares Se agregan un par de funciones para parsear un archivo de definicion de expresiones regulares y para extraer el contenido de una web como texto. Se prueba a crear listas mediante el list comprehension de python para generar los resultados al testear las expresiones regulares contra el contenido web.

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+import io
 import re
+import urllib3
 
 
-def compileRegex(pattern):
-    return re.compile(pattern, re.IGNORECASE)
+http = urllib3.PoolManager()
 
 
-def testSomeRegex(regex, text):
-    cre = compileRegex(regex)
-    match = cre.findall(text)
-    for m in match:
-        print(m)
+def getUrlContent(url):
+    return http.request('GET', url).data.decode('UTF-8')
+
+
+# La regex de este metodo pilla todas las lineas del estilo 're: este texto'
+def parseRegexFromFile(path):
+    with io.open(path, 'r', encoding='utf8') as f:
+        content = f.read()
+        r = re.compile(r're:\s{1}([^\s].*)', re.IGNORECASE)
+        return r.findall(content)
+
+
+def applyRegex(regex, content):
+    cr = re.compile(regex, re.IGNORECASE)
+    founded = cr.findall(content)
+    return [t[2] for t in founded]
 
 
 if __name__ == '__main__':
-    text = 'Habia una vez un cabron que vendia un huevo a 432,50 € el kilo'
-    testSomeRegex(r'\d+,?\d+?\s?€', text)
+    validRegex = parseRegexFromFile('res.txt')
+    content = getUrlContent('https://wetaca.com/175-crema-de-setas.html')
+    results = [applyRegex(r, content) for r in validRegex]
+
+    print(results)
