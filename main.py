a82da53f24fdbc0ce3a6323616be96050eb4068a
Gabriel Ferreiro <gbril9119@gmail.com>
2018-11-12 22:52:22 +0100

    Formateado de codigo Elimino el todo.md que ya no sirve pa nada y pongo el código algo más bonito.

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def captureCourseLinksInSoup(soup):
     links = []
     allLinks = [tag['href']
                 for tag in soup.find_all(class_=re.compile('lc_google_click'))]
+    # Eliminado duplicados. Podria usar un set pero me joderia el orden
+    # de la lista
     for l in allLinks:
         if l not in links:
             links.append(l)
@@ -25,11 +27,14 @@ def captureCourseLinksInSoup(soup):
 def parseCourse(courseLink):
     webContent = http.request('GET', courseLink).data.decode('UTF-8', 'ignore')
     soup = BeautifulSoup(webContent)
+
     courseLabels = [tag.get_text() for tag in soup.find_all(
         'span', attrs={'class': 'LC_name'})]
     courseValues = [tag.get_text() for tag in soup.find_all(
         'span', attrs={'class': 'LC_data'})]
+
     course = list(zip(courseLabels, courseValues))
+
     course.insert(0, ('Plato', soup.title.string.split('-')[0].strip()))
     return course
 
@@ -38,16 +43,22 @@ if __name__ == '__main__':
     wetacaMenuContent = http.request(
         'GET', 'https://wetaca.com/27-nuestros-platos').data.decode('UTF-8', 'ignore')
     soup = BeautifulSoup(wetacaMenuContent, 'html.parser')
+
     courseLinks = captureCourseLinksInSoup(soup)
+
+    # seguro que puede simplificarse para que no quede tan
+    # chapuza pero usando list comprehension el http peta
     courses = []
     for link in courseLinks:
         courses.append(parseCourse(link))
+
     sortedCourses = sorted(courses, key=lambda tup: tup[0])
 
     with open('wetaca-weekly.csv', 'w', encoding='utf8') as temp:
         fieldnames = dict(courses[0]).keys()
         writer = csv.DictWriter(temp, fieldnames=fieldnames)
         writer.writeheader()
+
         for c in courses:
             d = dict(c)
             writer.writerow(d)
--- todo.md
+++
@@ -1,3 +0,0 @@
-## Por hacer
-- En el archivo de definición de expresiones regulares estaría guay indicar que grupo de la
-  expresión es el que tiene que coger el programa
